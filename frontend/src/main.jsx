577aa997146fea7c22f007748064b80d9b64b8ac
Millena Nunes Cupolillo <126785826+Mihcup@users.noreply.github.com>
2025-10-06 19:12:44 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,7 +16,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles/Main.css';
 
-console.log('ID_CLIENTE:', import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
